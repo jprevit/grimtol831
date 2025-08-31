836b26e339854ccc6d042b63606d8a0d8e7b9e4f
John Previtera <johngprevitera@gmail.com>
2025-08-31 09:43:52 -0600

    fighting so many monsters

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
 // let shreknarHP = 3
 // let shreknarAP = 4
 
-let shreknar = {
+// SECTION Data
+
+let monster = {
+    name: 'Shreknar',
     hp: 3,
     ap: 4,
+    reward: 2,
+    picture: 'assets/Monsters/Goblin.png'
 }
+
+let monsters = [
+{
+    name: 'Tibbles',
+    hp: 6,
+    ap: 5,
+    reward: 4,
+    picture: 'assets/Monsters/Orc.png',
+}
+]
+
+// SECTION Game Logic
 
 function squishMonster(){
-
-    if(shreknar.hp > 0){
-
+    if(!monster.hp){
+        let newMonster = monsters.shift()
+        monster = newMonster
+    } else {
+        monster.hp -= 1
     }
-    shreknar.hp -= 1
-    console.log(shreknar.hp);
+    drawMonster()
+    console.log(monster.hp);
 }
+
+
+// SECTION Rendering
+
+function drawMonster(){
+    let monsterStats = document.getElementById('monster-stats')
+    let monsterPicture = document.getElementById('monster-picture')
+    let monsterName = document.getElementById('monster-name')
+
+    monsterPicture.setAttribute('src', `${monster.picture}`)
+    monsterName.innerText = monster.name
+
+    monsterStats.innerHTML = `<h2>HP: ${monster.hp}</h2>  <h2>Attack: ${monster.ap}</h2>`
+}
+
+
+drawMonster()
