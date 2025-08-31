c5c539d8a653a09676b6f99bd921ae94224d3111
John Previtera <johngprevitera@gmail.com>
2025-08-31 09:17:50 -0600

    Basic set u and squishing monsters

---
+++ app.js
@@ -0,0 +1,16 @@
+// let shreknarHP = 3
+// let shreknarAP = 4
+
+let shreknar = {
+    hp: 3,
+    ap: 4,
+}
+
+function squishMonster(){
+
+    if(shreknar.hp > 0){
+
+    }
+    shreknar.hp -= 1
+    console.log(shreknar.hp);
+}
